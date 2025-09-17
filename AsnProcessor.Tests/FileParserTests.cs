47e295d2bbeed6515c099617b596995df6c44b49
Rihards Dobelis <rihards.dobelis@gmail.com>
2025-09-18 02:02:35 +0300

    Add unit test for FileParser to validate handling of invalid lines

--- AsnProcessor.Tests/FileParserTests.cs
+++ AsnProcessor.Tests/FileParserTests.cs
@@ -11,16 +11,20 @@ public class FileParserTests
     {
         var fileParser = new FileParser();
         const string text = """
-                            HDR  TRSP117                                                                                     6874453I                           
-                            LINE P000001661                           9781473663800                     12     
-                            LINE P000001661                           9781473667273                     2      
-                            HDR  TRSP117                                                                                     6874454I                           
-                            LINE G000009810                           9781473662179                     8      
+                            HDR  TRSP117                                                                                     6874453I
+                            LINE P000001661                           9781473663800                     12
+                            LINE P000001661                           9781473667273                     2
+                            HDR  TRSP117                                                                                     6874454I
+                            LINE G000009810                           9781473662179                     8
                             """;
 
         await using var ms = new MemoryStream(Encoding.UTF8.GetBytes(text));
 
-        var boxes = await fileParser.ParseAsync(ms, CancellationToken.None).ToListAsync();
+        var boxes = new List<AsnProcessor.Domain.Entities.Box>();
+        await foreach (var box in fileParser.ParseAsync(ms, CancellationToken.None))
+        {
+            boxes.Add(box);
+        }
 
         boxes.Count.ShouldBe(2);
 
@@ -31,5 +35,28 @@ public class FileParserTests
         boxes[1].Identifier.ShouldBe("6874454I");
         boxes[1].Lines.Count.ShouldBe(1);
         boxes[1].Lines.First().Isbn.ShouldBe("9781473662179");
+    }
+
+    [Fact]
+    public async Task ParseAsync_WhenCalledWithInvalidLines_ShouldIgnoreInvalidLines()
+    {
+        var fileParser = new FileParser();
+        const string text = """
+                            HDR  TRSP117                                                                                     6874453I
+                            LINE P000001661                           9781473663800                     12
+                            INVALIDLINE something that does not match
+                            LINE P000001661                           9781473667273                     2
+                            """;
+
+        await using var ms = new MemoryStream(Encoding.UTF8.GetBytes(text));
+
+        var boxes = new List<AsnProcessor.Domain.Entities.Box>();
+        await foreach (var box in fileParser.ParseAsync(ms, CancellationToken.None))
+        {
+            boxes.Add(box);
+        }
+
+        boxes.Count.ShouldBe(1);
+        boxes[0].Lines.Count.ShouldBe(2);
     }
 }
